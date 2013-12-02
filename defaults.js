ba0feb6bfb9cb0bb358a6cccf0258496ef1768f0
Yaron Sheffer <yaronf.ietf@gmail.com>
2013-12-03 01:02:10 +0200

    As published.

---
+++ defaults.js
@@ -0,0 +1,6 @@
+document.clock_defaults = {"fullscreen_only": false,
+                "mil_time": true,
+                "fg_color": "#0000FF", // blue
+                "bg_color": "#FAF0E6", // linen
+                "bg_opacity": "0.4"
+};
